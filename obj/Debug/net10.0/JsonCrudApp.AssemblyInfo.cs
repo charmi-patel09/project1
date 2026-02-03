f51fec005e5b6305a7522b682a81fe9e3210727a
Charmi-marakana <pcharmi230@gmail.com>
2026-02-03 11:27:20 +0530

    Initial commit

--- obj/Debug/net10.0/JsonCrudApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/JsonCrudApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JsonCrudApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd1e497400917863fc23efbf4761c3d89ded545")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf7cdda4561c3e3bfda6069d151dde68280872a")]
 [assembly: System.Reflection.AssemblyProductAttribute("JsonCrudApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JsonCrudApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
